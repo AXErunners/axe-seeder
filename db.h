e7cb3650758f6e6ccf3ddacded0f017453279d10
-k <slowdive@me.com>
2018-07-08 11:45:36 -0400

    update proto and client version

--- db.h
+++ db.h
@@ -12,7 +12,7 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70208
+#define REQUIRE_VERSION 70210
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
@@ -119,7 +119,7 @@ public:
   }
   int GetBanTime() const {
     if (IsGood()) return 0;
-    if (clientVersion && clientVersion < REQUIRE_VERSION) { return 604800; }
+    if (clientVersion && clientVersion < REQUIRE_VERSION) { return 101050; }
     if (stat1M.reliability - stat1M.weight + 1.0 < 0.15 && stat1M.count > 32) { return 30*86400; }
     if (stat1W.reliability - stat1W.weight + 1.0 < 0.10 && stat1W.count > 16) { return 7*86400; }
     if (stat1D.reliability - stat1D.weight + 1.0 < 0.05 && stat1D.count > 8) { return 1*86400; }
