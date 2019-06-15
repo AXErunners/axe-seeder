a2e10fd61ef52290dfa5b2b087809f09cd46781a
-k <slowdive@me.com>
2019-06-15 12:45:13 -0700

    Update block height
    mainnet

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 40000 : 1000000;
+    return testnet ? 40000 : 275000;
 }
 
 std::string static inline ToString(const CService &ip) {
